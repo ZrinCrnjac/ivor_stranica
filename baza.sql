e383183ab6a5afb80d82465ab64f432c728870d3
Zrin Crnjac <zrin.crnjac@gmail.com>
2018-02-21 20:49:58 +0100

    Komentar tablica dodana u bazu

--- baza.sql
+++ baza.sql
@@ -10,6 +10,16 @@ create table igra(
 	studio varchar(20)
 );
 
+create table komentar(
+	tekst varchar(255),
+    igra_id int,
+    korisnik_id int
+);
+
+alter table komentar add foreign key (igra_id) references igra(igra_id);
+
+alter table komentar add foreign key (korisnik_id) references korisnik(korisnik_id);
+
 insert into korisnik(username,password) values ('Ivor','lozinka');
 
 insert into igra(ime,studio) values ('igra 1','studio 1'),('igra 2','studio 2'),('igra 3','studio 3'),('igra 4','studio 4'),('igra 5','studio 5');
