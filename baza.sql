996953c85f69bb7d9ff0edaf1bf12b4364548b63
Zrin Crnjac <zrin.crnjac@gmail.com>
2018-02-21 22:04:38 +0100

    Odustajem od komentara zasad

--- baza.sql
+++ baza.sql
@@ -11,6 +11,7 @@ create table igra(
 );
 
 create table komentar(
+	komentar_id int primary key not null auto_increment,
 	tekst varchar(255),
     igra_id int,
     korisnik_id int
@@ -23,3 +24,10 @@ alter table komentar add foreign key (korisnik_id) references korisnik(korisnik_
 insert into korisnik(username,password) values ('Ivor','lozinka');
 
 insert into igra(ime,studio) values ('igra 1','studio 1'),('igra 2','studio 2'),('igra 3','studio 3'),('igra 4','studio 4'),('igra 5','studio 5');
+
+INSERT INTO `komentar`(`tekst`, `igra_id`, `korisnik_id`) VALUES ('Odlicna igra 10/10',1,1),('Katastrofa 1/10',1,1);
+
+/*
+SELECT a.username, b.tekst, c.ime
+from korisnik a inner join komentar b where a.korisnik_id=b.korisnik_id
+inner join igra c where b.igra_id=c.igra_id order by a.korisnik_id desc
