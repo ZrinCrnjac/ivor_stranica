f6060de7f722ff4dce34a5f07f556939705fe2ab
Zrin Crnjac <zrin.crnjac@gmail.com>
2018-02-21 20:27:41 +0100

    CSS uspio pas mater

--- index.php
+++ index.php
@@ -6,16 +6,7 @@
 	</head>
 <body>
 	<?php include_once 'predlosci/izbornik.php'; ?>
-	<p>Početna</p>
-	<a href="igre.php">Igre</a>
-	<br>
-	<?php 
-	if(isset($_SESSION["logiran"])): ?>
-	<a href="logout.php">Logout</a>
-	<?php echo $_SESSION["logiran"]->username; ?>
-	<?php else: ?>
-	<a href="login.php">Login</a>
-	<a href="registracija.php">Registracija</a>
-	<?php endif; ?>
+	
+	<?php include_once 'predlosci/skripte.php'; ?>
 </body>
 </html>
--- predlosci/izbornik.php
+++ predlosci/izbornik.php
@@ -1,28 +1,23 @@
 <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
   <button class="menu-icon" type="button" data-toggle="example-menu"></button>
-  <div class="title-bar-title">Menu</div>
+  <div class="title-bar-title">Izbornik</div>
 </div>
 
 <div class="top-bar" id="example-menu">
   <div class="top-bar-left">
     <ul class="dropdown menu" data-dropdown-menu>
-      <li class="menu-text">Site Title</li>
-      <li>
-        <a href="#">One</a>
-        <ul class="menu vertical">
-          <li><a href="#">One</a></li>
-          <li><a href="#">Two</a></li>
-          <li><a href="#">Three</a></li>
-        </ul>
-      </li>
-      <li><a href="#">Two</a></li>
-      <li><a href="#">Three</a></li>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="igre.php">Games</a></li>
     </ul>
   </div>
   <div class="top-bar-right">
     <ul class="menu">
-      <li><input type="search" placeholder="Search"></li>
-      <li><button type="button" class="button">Search</button></li>
+    	<li><?php if(!isset($_SESSION["logiran"])): ?>
+    	<a href="login.php"><button type="button" class="button">Login</button></a>
+    	<?php else: ?>
+    	<a href="logout.php" class="alert button expanded">Logout 
+      		<?php echo $_SESSION["logiran"]->username; ?></a>
+    	<?php endif; ?></li>
     </ul>
   </div>
 </div>
