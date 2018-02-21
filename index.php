00f91772d6b6b28aee2992cc850a1b97ce2ab7df
Zrin Crnjac <zrin.crnjac@gmail.com>
2018-02-21 19:59:01 +0100

    CSS 1.0

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php include_once 'konfiguracija.php' ; ?>
 <!DOCTYPE html>
 <html>
+	<head>
+	<?php include_once 'predlosci/head.php'; ?>
+	</head>
 <body>
+	<?php include_once 'predlosci/izbornik.php'; ?>
 	<p>Početna</p>
 	<a href="igre.php">Igre</a>
 	<br>
